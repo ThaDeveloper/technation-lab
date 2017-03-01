58e45ef28da114bc25af6a314c13264dd6be125c
Justin Ndwiga <ndwigajustin@gmail.com>
2017-03-01 23:34:54 +0300

    Update ngamia.rb

--- ngamia.rb
+++ ngamia.rb
@@ -1,9 +1,9 @@
 s="my-variable-name"
 array = s.split("-")
   puts array.map {|s|
-    if array.length(s) >= 1
+    if array.length() >= 1
           s.capitalize!
     else
       s
     end
-  }y.join
+}.join()
